a9a5c7ed9c59c1412e6a8c36a7be0207a67d0cd0
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:25:20 -0700

    Fix few errors in functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Create an object that holds the methods that will be shared by all instances of the class
+  // Use the keyword this in your methods
+  // Use _.extend to copy the methods onto the instance
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    if (this.count < 0) {
+      this.count = 0;
+    }
+    return this.count;    
+  },
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    this.count--;
+    return this.storage[this.count];
+  }
+};
 
 
