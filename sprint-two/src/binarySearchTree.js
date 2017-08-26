b727c3705fdb8b2381fa0dba54d7f8cbdd72fc39
Hack Reactor Student <student@hackreactor.com>
2017-08-25 21:51:22 -0700

    working on BTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,88 @@
 var BinarySearchTree = function(value) {
+  var aTree = Object.create(treeMethodz);
+  aTree.value = value;
+  aTree.right = null;
+  aTree.left = null;
+  return aTree;
 };
 
+var treeMethodz = {};
 
+treeMethodz.insert = function(val) {
+  if ( val < this.value && this.left === null ) {
+    this.left = BinarySearchTree(val);
+  } else if ( val < this.value && this.left !== null ) {
+    this.left.insert(val);
+  } else if ( val > this.value && this.right === null ) {
+    this.right = BinarySearchTree(val);
+  } else if ( val > this.value && this.right !== null ) {
+    this.right.insert(val);    
+  }
+};
+
+treeMethodz.contains = function(target) {
+  if ( this.value === target) {
+    return true;
+  } 
+
+  if (target < this.value && this.left !== null) {
+    return this.left.contains(target);
+  }  
+
+  if (target > this.value && this.right !== null) {
+    return this.right.contains(target);
+  }
+  return false;
+};
+
+treeMethodz.depthFirstLog = function(cb) {
+  for (var key in this.value) {
+    cb(key);
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+// var Tree = function(value) {
+//   var newTree = {};
+//   newTree.value = value;
+
+//   // your code here
+//   newTree.children = [];  // fix me
+//   _.extend(newTree, treeMethods);
+//   return newTree;
+// };
+
+// var treeMethods = {};
+
+// treeMethods.addChild = function(value) {
+//   this.children.push(Tree(value));
+// };
+
+// treeMethods.contains = function(target) {
+// // base case
+
+//   if ( this.value === target) { 
+//     return true;
+//   }
+
+// // recursive 
+//   if (this.children.length > 0 ) {
+//     for ( var i = 0; i < this.children.length; i++ ) {
+//       if (this.children[i].contains(target)) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false; 
+// };
+
+// tree = { value: Null, children: 
+// [ { value: 5, children: [ { value: 7, children: [] } ] }, 
+//{ value: 6, children: [{ value: 8, children: [] }]}]}
