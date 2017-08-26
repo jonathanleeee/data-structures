c7dc53d8fde56ccc20e60c2662884ee13047641f
Hack Reactor Student <student@hackreactor.com>
2017-08-26 15:51:39 -0700

    Finished hash tree

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,30 +7,60 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // get you the index, generate a index for you
+  var bucket = this._storage.get(index) || [];
+  // if it's there, get you the bucket. if not, you make a new bucket
+  for ( var i = 0; i < bucket.length; i++) {
+  // [ [k, v], [k, v] ]
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      return;
+    }
+  }
+  // if previous bucket didnt have value, just push a new tuple
+  bucket.push([k, v]);
+  this._storage.set(index, bucket);
 
-  if (this._storage.get(index)) {
-    var change = false;
-    this._storage.each(function(itemArr) {
-      if (itemArr[0] === k) {
-        this._storage.set(index, [k, v]);
-        change = true;
-      }
-    });
-    if (change = false) {
-      this._storage[index].push([k, v]);
-    } 
-  } else {
-    this._storage[index] = [[k, v]];
-  } 
+
+
+
+
+  // if (this._storage.get(index)) {
+  //   var change = false;
+  //   this._storage.each(function(itemArr) {
+  //     if (itemArr[0] === k) {
+  //       this._storage.set(index, [k, v]);
+  //       change = true;
+  //     }
+  //   });
+  //   if (change = false) {
+  //     this._storage[index].push([k, v]);
+  //   } 
+  // } else {
+  //   this._storage[index] = [[k, v]];
+  // } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      return this._storage[index][i][1];
+
+  var bucket = this._storage.get(index);
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i][0] === k) {
+      return bucket[i][1];
     }
   }
+  
+
+
+
+
+  // for (var i = 0; i < this._storage[index].length; i++) {
+  //   if (this._storage[index][i][0] === k) {
+  //     return this._storage[index][i][1];
+  //   }
+  // }
 };
 
 HashTable.prototype.remove = function(k) {
@@ -38,11 +68,21 @@ HashTable.prototype.remove = function(k) {
   // going through the storage in each index in storage
   //
   //debugger;
-  for ( var i = 0; i < this._storage[index].length; i++ ) {
-    if (this._storage[index][i][0] === k) {
-      this._storage[index].splice(i, 1);
+  var bucket = this._storage.get(index);
+  for ( var i = 0; i < bucket.length; i++ ) {
+    if ( bucket[i][0] === k) {
+      bucket.splice(i, 1);
     }
   }
+  //this._storage.set(index, bucket);  
+
+
+
+  // for ( var i = 0; i < this._storage[index].length; i++ ) {
+  //   if (this._storage[index][i][0] === k) {
+  //     this._storage[index].splice(i, 1);
+  //   }
+  // }
 
 
 
